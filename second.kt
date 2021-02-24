fe2b91e0b4ad50f7b68d009a6914ccd360ff7c18
Ariq Andrean <79554778+Festezy@users.noreply.github.com>
2021-02-24 11:50:12 +0700

    Update second.kt

--- second.kt
+++ second.kt
@@ -5,6 +5,8 @@ fun main() {
     val bin = "101101"
 
 
+    // using kotlin feature in it
+    
     for((x, i) in (bin.length downTo 1 step 1).withIndex()){
         dec += (bin[x].toString().toInt() * 2.0.pow(i.toDouble() - 1).toInt())
         println("${bin[x].toString().toInt()} * ${2.0.pow(i.toDouble() - 1).toInt()} = " +
