9ccd0d08765c5e2e351296a8cedf724827a36ebb
Ariq Andrean <79554778+Festezy@users.noreply.github.com>
2021-02-24 11:46:50 +0700

    Create second.kt

---
+++ second.kt
@@ -0,0 +1,15 @@
+import kotlin.math.pow
+
+fun main() {
+    var dec = 0
+    val bin = "101101"
+
+
+    for((x, i) in (bin.length downTo 1 step 1).withIndex()){
+        dec += (bin[x].toString().toInt() * 2.0.pow(i.toDouble() - 1).toInt())
+        println("${bin[x].toString().toInt()} * ${2.0.pow(i.toDouble() - 1).toInt()} = " +
+                "${bin[x].toString().toInt()} * ${2.0.pow(i.toDouble() - 1).toInt()}")
+    }
+
+    println("The Decimal from Binary $bin is $dec")
+}
